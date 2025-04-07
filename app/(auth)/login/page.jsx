95b82b7430e4354154046fee53adf0d1e44f52d2
tobacoguys <dungchelsea1723@gmail.com>
2025-04-08 02:50:31 +0700

    add sign in button

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { Button } from "@nextui-org/react";
+
 export default function Login() {
     return (
         <main className="w-full flex justify-center items-center bg-gray-300 p-24 min-h-screen ">
@@ -9,21 +13,24 @@ export default function Login() {
                     <h1 className="font-bold text-xl">
                         Login With Email
                     </h1>
-                    <form >
+                    <form className="flex flex-col gap-3">
                         <input 
                             placeholder="Enter you email" 
                             type="email" 
                             name="user-email" 
                             id="user-email"
-                            className="px-3 py-2 rounded border focus:outline-none w-full"
-                        />{""}
+                            className="px-3 py-2 rounded-xl border focus:outline-none w-full"
+                        />
                         <input 
                             placeholder="Enter you password" 
                             type="password" 
                             name="user-password" 
                             id="user-password"
-                            className="px-3 py-2 rounded border focus:outline-none w-full"
-                        />{""}
+                            className="px-3 py-2 rounded-xl border focus:outline-none w-full"
+                        />
+                        <Button color="primary" className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-xl">
+                            Login
+                        </Button>
                     </form>
                 </div>
             </section>
