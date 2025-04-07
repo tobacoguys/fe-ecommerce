c960f53d2b83f9fd5775098ff6d9310d85bf84e9
tobacoguys <dungchelsea1723@gmail.com>
2025-04-08 03:27:29 +0700

    improve Login page responsive

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -5,12 +5,12 @@ import Link from "next/link";
 
 export default function Login() {
     return (
-        <main className="w-full flex justify-center items-center bg-gray-300 p-24 min-h-screen ">
+        <main className="w-full flex justify-center items-center bg-gray-300 md:p-24 min-h-screen ">
             <section className="flex flex-col gap-3">
                 <div className="flex justify-center">
                     <img className="h-12" src="/logo.png" alt="Logo" />
                 </div>
-                <div className="flex flex-col gap-3 bg-white p-10 rounded-xl min-w-[440px]">
+                <div className="flex flex-col gap-3 bg-white md:p-10 p-5 rounded-xl md:min-w-[440px] w-full">
                     <h1 className="font-bold text-xl">
                         Login With Email
                     </h1>
