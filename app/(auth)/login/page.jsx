973b223ffe19019e25d94a7d54f1271a8026d93b
tobacoguys <dungchelsea1723@gmail.com>
2025-04-08 01:18:06 +0700

    Create styled Login page with layout and email input field

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,9 +1,25 @@
 export default function Login() {
     return (
-        <main>
-            <h1>
-                Login Page
-            </h1>
+        <main className="w-full flex justify-center items-center bg-gray-300 p-24 min-h-screen ">
+            <section className="flex flex-col gap-3">
+                <div className="flex justify-center">
+                    <img className="h-12" src="/logo.png" alt="Logo" />
+                </div>
+                <div className="flex flex-col gap-3 bg-white p-10 rounded-xl min-w-[440px]">
+                    <h1 className="font-bold text-xl">
+                        Login With Email
+                    </h1>
+                    <form >
+                        <input 
+                            placeholder="Enter you email" 
+                            type="email" 
+                            name="user-email" 
+                            id="user-email"
+                            className="px-3 py-2 rounded border focus:outline-none w-full"
+                        />{""}
+                    </form>
+                </div>
+            </section>
         </main>
     );
 }
