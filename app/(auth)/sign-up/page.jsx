b93fc391dc26db24b40f917cb03c1a7de7c334c6
tobacoguys <dungchelsea1723@gmail.com>
2025-04-17 22:08:06 +0700

    Create sign-up page & add logo

---
+++ app/(auth)/sign-up/page.jsx
@@ -0,0 +1,11 @@
+export default function Page(){
+    return (
+        <main className="w-full flex justify-center items-center bg-gray-300 md:p-24 min-h-screen ">
+            <section className="flex flex-col gap-3">
+                <div className="flex justify-center">
+                    <img className="h-12" src="/logo.png" alt="Logo" />
+                </div>
+            </section>
+        </main>
+    )
+}
