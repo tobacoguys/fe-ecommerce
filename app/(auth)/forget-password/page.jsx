c03a898d9a86a6e418b8cab996ed829955cc6e56
tobacoguys <dungchelsea1723@gmail.com>
2025-04-18 16:24:09 +0700

    Enter email input

--- app/(auth)/forget-password/page.jsx
+++ app/(auth)/forget-password/page.jsx
@@ -5,6 +5,19 @@ export default function Page() {
                 <div className="flex justify-center">
                     <img className="h-12" src="/logo.png" alt="Logo" />
                 </div>
+
+                <div className="flex flex-col gap-3 bg-white md:p-10 p-5 rounded-xl md:min-w-[440px] w-full">
+                    <h1 className="font-bold text-xl">Forgot Password</h1>
+                    <form>
+                        <input
+                            placeholder="Enter Your Email"
+                            type="email"
+                            name="user-email"
+                            id="user-email"
+                            className="px-3 py-2 rounded-xl border focus:outline-none w-full"
+                        />
+                    </form>
+                </div>
             </section>
         </main>
     )
